8fa9f9ccf671af9abfe74d513cfcf0a4a7dff0e5
ghieu123 <giahieuc5@gmail.com>
2024-07-29 13:12:13 +0700

    Cau 3,6

---
+++ Bài tập về nhà/Cau 3,5.cpp
@@ -0,0 +1,41 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+vector<vector<int>> matrix = {
+    {13, 27, 35, 42},
+    {56, 22, 91, 18},
+    {45, 33, 55, 67},
+    {89, 76, 23, 90}
+};
+
+// 5. Sắp xếp các dòng có chỉ số lẻ tăng dần, chỉ số chẵn giảm dần
+vector<vector<int>> sort_rows(vector<vector<int>>& matrix) {
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        if (i % 2 == 0) {
+            sort(matrix[i].begin(), matrix[i].end(), greater<int>());
+        }
+        else {
+            sort(matrix[i].begin(), matrix[i].end());
+        }
+    }
+    return matrix;
+}
+
+
+int main() {
+    // Sắp xếp các dòng
+    auto sorted_rows = sort_rows(matrix);
+    cout << "Sap xep lai cac dong:" << endl;
+    for (const auto& row : sorted_rows) {
+        for (int num : row) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ Bài tập về nhà/Cau 3,6.cpp
@@ -0,0 +1,50 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+vector<vector<int>> matrix = {
+    {13, 27, 35, 42},
+    {56, 22, 91, 18},
+    {45, 33, 55, 67},
+    {89, 76, 23, 90}
+};
+
+// 6. Sắp xếp các cột có chỉ số lẻ giảm dần, chỉ số chẵn tăng dần
+vector<vector<int>> sort_columns(vector<vector<int>>& matrix) {
+    size_t rows = matrix.size();
+    size_t cols = matrix[0].size();
+    for (size_t j = 0; j < cols; ++j) {
+        vector<int> col(rows);
+        for (size_t i = 0; i < rows; ++i) {
+            col[i] = matrix[i][j];
+        }
+        if (j % 2 == 0) {
+            sort(col.begin(), col.end());
+        }
+        else {
+            sort(col.begin(), col.end(), greater<int>());
+        }
+        for (size_t i = 0; i < rows; ++i) {
+            matrix[i][j] = col[i];
+        }
+    }
+    return matrix;
+}
+
+
+int main() {
+    // Sắp xếp các cột
+    auto sorted_cols = sort_columns(matrix);
+    cout << "Sap xep lai cac cot:" << endl;
+    for (const auto& row : sorted_cols) {
+        for (int num : row) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
