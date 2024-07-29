978f2ccbdaa8bc1229c28ca107d5dd0bea8d1b3e
ghieu123 <giahieuc5@gmail.com>
2024-07-29 13:02:27 +0700

    Cau 3,3

---
+++ Bài tập về nhà/Cau 3,3.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+vector<vector<int>> matrix = {
+    {13, 27, 35, 42},
+    {56, 22, 91, 18},
+    {45, 33, 55, 67},
+    {89, 76, 23, 90}
+};
+
+// 3. Đếm số phần tử có chứa chữ số 2
+int count_elements_with_2(const vector<vector<int>>& matrix) {
+    int count = 0;
+    for (const auto& row : matrix) {
+        for (int num : row) {
+            string num_str = to_string(num);
+            count += count_if(num_str.begin(), num_str.end(), [](char c) { return c == '2'; });
+        }
+    }
+    return count;
+}
+
+
+int main() {
+    // Đếm số phần tử có chứa chữ số 2
+    cout << "Dem so phan tu co chua chu so '2': " << count_elements_with_2(matrix) << endl;
+
+    return 0;
+}
